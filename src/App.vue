6b7e0640efb4f6de2d6860718f9ba2ee6d1aaaff
c249004 <c249004@ivy.ac.jp>
2025-06-25 14:15:41 +0900

    0625_1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>⚾ 球場お天気アプリ</h1>
+    <BaseballMap />
+  </div>
+</template>
+
+<script>
+import BaseballMap from './components/BaseballMap.vue';
+
+export default {
+  components: {
+    BaseballMap
+  }
+};
+</script>
+
+<style>
+/* 必要なら見た目のスタイルをここに書いてOK */
+</style>
---
+++ src/components/BaseballMap.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="map-wrapper">
+    <img src="@/assets/japan-map.png" class="map-image" />
+
+    <div
+      v-for="stadium in stadiums"
+      :key="stadium.name"
+      class="marker"
+      :style="{ top: stadium.top + 'px', left: stadium.left + 'px' }"
+      @click="selectStadium(stadium)"
+    >
+      ⚾
+    </div>
+
+    <p v-if="selectedStadium">選んだ球場：{{ selectedStadium.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stadiums: [
+        { name: "東京ドーム", top: 250, left: 310 },
+        { name: "甲子園", top: 380, left: 200 },
+        // ここに他の球場も追加可能
+      ],
+      selectedStadium: null
+    };
+  },
+  methods: {
+    selectStadium(stadium) {
+      this.selectedStadium = stadium;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.map-wrapper {
+  position: relative;
+  width: 600px; /* 地図の幅に合わせて調整 */
+}
+
+.map-image {
+  width: 100%;
+}
+
+.marker {
+  position: absolute;
+  cursor: pointer;
+  font-size: 20px;
+  transform: translate(-50%, -50%);
+}
+</style>
